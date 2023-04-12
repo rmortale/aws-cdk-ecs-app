022a7244bdfd08deb3a92b21c4963bf40dc27795
Antonio Dolce <dolce.antonio@gmail.com>
2023-04-12 22:37:23 +0200

    Deploy ECR repo

--- cdk/src/main/java/com/myorg/BootstrapApp.java
+++ cdk/src/main/java/com/myorg/BootstrapApp.java
@@ -12,29 +12,29 @@ import software.amazon.awscdk.StackProps;
  */
 public class BootstrapApp {
 
-  public static void main(final String[] args) {
-    App app = new App();
+    public static void main(final String[] args) {
+        App app = new App();
 
-    String region = (String) app.getNode().tryGetContext("region");
-    Validations.requireNonEmpty(region, "context variable 'region' must not be null");
+        String region = (String) app.getNode().tryGetContext("region");
+        Validations.requireNonEmpty(region, "context variable 'region' must not be null");
 
-    String accountId = (String) app.getNode().tryGetContext("accountId");
-    Validations.requireNonEmpty(accountId, "context variable 'accountId' must not be null");
+        String accountId = (String) app.getNode().tryGetContext("accountId");
+        Validations.requireNonEmpty(accountId, "context variable 'accountId' must not be null");
 
-    Environment awsEnvironment = makeEnv(accountId, region);
+        Environment awsEnvironment = makeEnv(accountId, region);
 
-    Stack bootstrapStack = new Stack(app, "Bootstrap", StackProps.builder()
-      .env(awsEnvironment)
-      .build());
+        Stack bootstrapStack = new Stack(app, "Bootstrap", StackProps.builder()
+                .env(awsEnvironment)
+                .build());
 
-    app.synth();
-  }
+        app.synth();
+    }
 
-  static Environment makeEnv(String account, String region) {
-    return Environment.builder()
-      .account(account)
-      .region(region)
-      .build();
-  }
+    static Environment makeEnv(String account, String region) {
+        return Environment.builder()
+                .account(account)
+                .region(region)
+                .build();
+    }
 
 }
---
+++ cdk/src/main/java/com/myorg/DockerRepositoryApp.java
@@ -0,0 +1,52 @@
+package com.myorg;
+
+import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ecr.LifecycleRule;
+import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.iam.AccountPrincipal;
+
+import java.util.Collections;
+
+public class DockerRepositoryApp {
+
+    public static void main(String[] args) {
+        App app = new App();
+
+        String accountId = (String) app.getNode().tryGetContext("accountId");
+        Validations.requireNonEmpty(accountId, "context variable 'accountId' must not be null");
+
+        String region = (String) app.getNode().tryGetContext("region");
+        Validations.requireNonEmpty(region, "context variable 'region' must not be null");
+
+        String applicationName = (String) app.getNode().tryGetContext("applicationName");
+        Validations.requireNonEmpty(applicationName, "context variable 'applicationName' must not be null");
+
+        Environment awsEnvironment = makeEnv(accountId, region);
+
+        Stack dockerRepositoryStack = new Stack(app, "DockerRepositoryStack", StackProps.builder()
+                .stackName(applicationName + "-DockerRepository")
+                .env(awsEnvironment)
+                .build());
+
+        Repository ecrRepository = Repository.Builder.create(dockerRepositoryStack, "ecrRepository")
+                .repositoryName(applicationName + "-Repository")
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .lifecycleRules(Collections.singletonList(LifecycleRule.builder()
+                        .rulePriority(1)
+                        .description("limit to 10 images")
+                        .maxImageCount(10)
+                        .build()))
+                .build();
+
+        // grant pull and push to all users of the account
+        ecrRepository.grantPullPush(new AccountPrincipal(accountId));
+    }
+
+
+    static Environment makeEnv(String account, String region) {
+        return Environment.builder()
+                .account(account)
+                .region(region)
+                .build();
+    }
+}
